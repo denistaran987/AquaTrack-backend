994377065bca1012f63783a785ecc0e45736da4f
Denis <denistaran482@gmail.com>
2025-03-07 14:19:12 +0200

    fix auth

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -2,21 +2,15 @@ import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
-import { loginUserController, logoutUserController, registerUserController } from '../controllers/auth.js';
+import {
+  loginUserController,
+  logoutUserController,
+  registerUserController,
+} from '../controllers/auth.js';
 
 const authRouter = Router();
-authRouter.post(
-    '/register',
-    validateBody(registerUserSchema),
-    ctrlWrapper(registerUserController)
-);
-authRouter.post(
-    '/login',
-    validateBody(loginUserSchema),
-    ctrlWrapper(loginUserController));
-authRouter.post(
-    '/logout',
-    ctrlWrapper(logoutUserController)
-);
+authRouter.post('/signup', validateBody(registerUserSchema), ctrlWrapper(registerUserController));
+authRouter.post('/signin', validateBody(loginUserSchema), ctrlWrapper(loginUserController));
+authRouter.post('/logout', ctrlWrapper(logoutUserController));
 
 export default authRouter;
---
+++ src/utils/setupSession.js
@@ -0,0 +1,13 @@
+import { ONE_MONTH } from '../constants/times/constants.js';
+
+export const setupSession = (session, res) => {
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_MONTH),
+  });
+
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_MONTH),
+  });
+};
