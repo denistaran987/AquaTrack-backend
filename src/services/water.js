04b42b130d5b4e87ff347ff34778cabe71e4570d
Denis <denistaran482@gmail.com>
2025-03-07 17:33:26 +0200

    remove fake data

--- src/services/water.js
+++ src/services/water.js
@@ -1,4 +1,5 @@
 import expressAsyncHandler from 'express-async-handler';
+import { Water } from '../db/models/water.js';
 
 export const getDayWaterService = expressAsyncHandler(async (req, res) => {
   const { _id: owner } = req.user;
@@ -20,38 +21,13 @@ export const getDayWaterService = expressAsyncHandler(async (req, res) => {
   endOfDay.setUTCHours(23, 59, 59, 999);
   endOfDay.setMinutes(endOfDay.getMinutes() - userTimezoneOffset);
 
-  // Цей код пріоритетний і його потрібно розкоментувати, коли видалимо фікційні дані- масив обʼєктів нижче foundWaterDayData
-
-  // const foundWaterDayData = await Water.find({
-  //   owner,
-  //   date: {
-  //     $gte: startOfDay,// Включає всі записи, починаючи з 00:00
-  //     $lt: endOfDay, // Включає всі записи ДО 23:59:59.999
-  //   },
-  // });
-
-  // Фікційні дані для тестування, потрібно буде пізніше видалити
-  const foundWaterDayData = [
-    {
-      _id: 'mock123',
-      date: new Date().toISOString(),
-      amount: 500,
+  const foundWaterDayData = await Water.find({
+    owner,
+    date: {
+      $gte: startOfDay, // Включає всі записи, починаючи з 00:00
+      $lt: endOfDay, // Включає всі записи ДО 23:59:59.999
     },
-    {
-      _id: 'mock124',
-      date: new Date().toISOString(),
-      amount: 300,
-    },
-  ];
-
-  if (!foundWaterDayData.length) {
-    return {
-      date,
-      totalDayWater: 0,
-      consumedWaterData: [],
-      owner,
-    };
-  }
+  });
 
   // Обчислення обʼєму води випитої за день
   // Створення масиву даних про спожиту воду
@@ -80,33 +56,13 @@ export const getMonthWaterService = expressAsyncHandler(async (req, res) => {
   // Визначаємо кінець місяця
   const endOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
-  // Цей код пріоритетний і його потрібно розкоментувати, коли видалимо фікційні дані- масив обʼєктів foundWaterMonthData
-  // const foundWaterMonthData = await Water.find({
-  //   owner,
-  //   date: {
-  //     $gte: startOfMonth,
-  //     $lt: endOfMonth,
-  //   },
-  // });
-
-  // Фікційні дані для тестування, потрібно буде пізніше видалити
-  const foundWaterMonthData = [
-    {
-      _id: 'mock125',
-      date: new Date('2024-03-01T10:00:00.000Z'),
-      amount: 500,
+  const foundWaterMonthData = await Water.find({
+    owner,
+    date: {
+      $gte: startOfMonth,
+      $lt: endOfMonth,
     },
-    {
-      _id: 'mock126',
-      date: new Date('2024-03-02T12:00:00.000Z'),
-      amount: 300,
-    },
-    {
-      _id: 'mock127',
-      date: new Date('2024-03-02T18:00:00.000Z'),
-      amount: 700,
-    },
-  ];
+  });
 
   const summarizedData = foundWaterMonthData.reduce((acc, item) => {
     const date = new Date(item.date);
