f792012dcbe430d5132b81ddf9aff7c23ef812f5
Olena Borodkina <167635386+OlenkaOB@users.noreply.github.com>
2025-03-09 00:46:34 +0200

    total-users (#14)

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,4 @@
-import { loginUser, logoutUser, registerUser } from '../services/auth.js';
+import { countUsers, loginUser, logoutUser, registerUser } from '../services/auth.js';
 import { setupSession } from '../utils/setupSession.js';
 
 export const registerUserController = async (req, res) => {
@@ -32,4 +32,16 @@ export const logoutUserController = async (req, res) => {
   res.clearCookie('refreshToken');
 
   res.status(204).send();
+};
+
+export const countUsersController = async (req, res) => {
+  const totalUsers = await countUsers();
+  res.json({
+    status: 200,
+    message: 'Total number of registred users retrieved successfully!',
+    data: { totalUsers },
+  });
+
+
+
 };
