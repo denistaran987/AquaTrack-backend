439000f5c75cce95f58068e13473c81cea435fbb
Denis <denistaran482@gmail.com>
2025-03-08 18:04:56 +0200

    fix register user

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,24 +1,58 @@
 import { model, Schema } from 'mongoose';
 
-
 const usersSchema = new Schema(
-    {
-        name: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-
+  {
+    name: {
+      type: String,
+      default: ' ',
+      required: true,
     },
-    {
-        timestamps: true,
-        versionKey: false,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
+    password: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ['male', 'female'],
+      default: 'female',
+      required: true,
+    },
+    weight: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    dailySportTime: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    dailyNorm: {
+      type: Number,
+      default: 1500,
+      required: true,
+    },
+    avatarUrl: {
+      type: String,
+      default: ' ',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
 );
 
 usersSchema.methods.toJSON = function () {
-    const obj = this.toObject();
-    delete obj.password;
-    return obj;
-
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
 };
 
 export const UsersCollection = model('user', usersSchema);
