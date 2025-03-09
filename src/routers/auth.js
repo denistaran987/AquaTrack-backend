001f3c7901c73a46503df999a139ed6773ffdeab
Olena Borodkina <167635386+OlenkaOB@users.noreply.github.com>
2025-03-09 23:46:07 +0200

    reset-email-password (#17)
    * reset-email-password
    
    * template

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,18 +1,39 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
+import { loginUserSchema, registerUserSchema, resetEmailSchema, resetPasswordSchema } from '../validation/auth.js';
 import {
   countUsersController,
   loginUserController,
   logoutUserController,
   registerUserController,
+  resetEmailController,
+  resetPasswordController,
 } from '../controllers/auth.js';
 
 const authRouter = Router();
-authRouter.post('/signup', validateBody(registerUserSchema), ctrlWrapper(registerUserController));
-authRouter.post('/signin', validateBody(loginUserSchema), ctrlWrapper(loginUserController));
-authRouter.post('/logout', ctrlWrapper(logoutUserController));
-authRouter.get('/totalUsers', ctrlWrapper(countUsersController));
+authRouter.post(
+  '/signup',
+  validateBody(registerUserSchema),
+  ctrlWrapper(registerUserController));
+authRouter.post(
+  '/signin',
+  validateBody(loginUserSchema),
+  ctrlWrapper(loginUserController));
+authRouter.post(
+  '/logout',
+  ctrlWrapper(logoutUserController));
+authRouter.get(
+  '/totalUsers',
+  ctrlWrapper(countUsersController));
+authRouter.post(
+  '/send-reset-email',
+  validateBody(resetEmailSchema),
+  ctrlWrapper(resetEmailController));
+authRouter.post(
+  '/reset-pwd',
+  validateBody(resetPasswordSchema),
+  ctrlWrapper(resetPasswordController),
+);
 
 export default authRouter;
